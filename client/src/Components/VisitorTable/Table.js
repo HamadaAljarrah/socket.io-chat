9b300ff6d3d219fcafd26b134c606206215ff70e
Hamada <Hamadasepost@gmail.com>
2022-06-22 21:49:14 +0200

    some clean up, need to fix a bug

--- client/src/Components/VisitorTable/Table.js
+++ client/src/Components/VisitorTable/Table.js
@@ -1,59 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import axios from "axios"
+import React from 'react'
 import classes from "./Table.module.css"
 import TableRow from './TableRow'
-import { socket } from '../../scoket'
+import { HandleUpdate } from './table-logics'
+
 export default function Table() {
-    const [visitors, setVisitors] = useState([]);
 
-    const getVisitor = async () => {
-        const { data } = await axios("https://ipapi.co/json/");
-        const visitor = {
-            ip: data.ip,
-            city: data.city,
-            region: data.region,
-            flag: `https://countryflagsapi.com/png/${data.country_code}`,
-            country: data.country_name, 
-        }
-        socket.emit("newVisitor", visitor);
-
-        socket.on("newVisitor", (data)=>{
-            setVisitors(data);
-        })
-        socket.on("deleteVisitor", (data)=>{
-            setVisitors(data);
-        })
-    };
-
-    useEffect(()=>{getVisitor()},[])
-
-  return (
-    <>
-        <h1>Online Users<div className={classes.greenDot}></div></h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>IP</th>
-                    <th>Flag</th>
-                    <th>Country</th>
-                    <th>Region</th>
-                    <th>City</th>
-                </tr>
-            </thead>
-            <tbody>
-                {visitors.map((visitor, index)=><TableRow
-                    key={index}
-                    index={index +1}
-                    ip={visitor.ip}
-                    country={visitor.country}
-                    flag={visitor.flag}
-                    region={visitor.region}
-                    city={visitor.city}
-                />)}
-            </tbody>
-        </table>
-    </>
-    
-  )
-}
+    let {visitors} = HandleUpdate()
+    return (
+        <>
+            <h1>Online Users<div className={classes.greenDot}></div></h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>IP</th>
+                        <th>Flag</th>
+                        <th>Country</th>
+                        <th>Region</th>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {visitors.map((visitor, index)=><TableRow
+                        key={index}
+                        index={index +1}
+                        ip={visitor.ip}
+                        country={visitor.country}
+                        flag={visitor.flag}
+                        region={visitor.region}
+                        city={visitor.city}
+                    />)}
+                </tbody>
+            </table>
+        </>
+        
+    )
+    }
---
+++ client/src/Components/VisitorTable/table-logics.js
@@ -0,0 +1,47 @@
+import {useEffect, useState} from 'react'
+import axios from "axios"
+import { socket } from '../../scoket'
+
+
+
+const filterTableUsers = (data)=>{
+    const d = data
+    d.forEach((el, index) => {
+        for (let i = 0; i < data.length; i++) {
+            if(i !== index) {
+                if (el.id === data[i].id){
+                    data.splice(i, 1)
+                }
+            }
+        }
+    })
+    return d;
+}
+
+
+
+export const HandleUpdate = ()=>{
+    const [visitors, setVisitors] = useState([]);
+
+    const getVisitor = async () => {
+        const { data } = await axios("https://ipapi.co/json/");
+        const visitor = {
+            ip: data.ip,
+            city: data.city,
+            region: data.region,
+            flag: `https://countryflagsapi.com/png/${data.country_code}`,
+            country: data.country_name, 
+        }
+
+        socket.emit("newVisitor", visitor);
+        socket.on("newVisitor", (data)=>{
+            const user = filterTableUsers(data);
+            setVisitors(user);
+            console.log(user);
+        })
+        socket.on("deleteVisitor", data=>setVisitors(data))
+    };
+    useEffect(()=>{getVisitor()},[])
+    return {visitors}
+}
+
